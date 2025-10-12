a9b5a61ab3b724beba1e925607af74f10d354fd3
casalecchi <felipevilela191@gmail.com>
2025-10-12 18:31:32 -0300

    create xcode project

---
+++ Protfolio Project/Protfolio_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Protfolio_ProjectApp.swift
+//  Protfolio Project
+//
+//  Created by Felipe Casalecchi on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Protfolio_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
