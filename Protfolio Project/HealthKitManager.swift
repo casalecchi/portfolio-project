679c8c587cc612b4397819b301012ee95e4ac186
casalecchi <felipevilela191@gmail.com>
2025-10-25 20:06:01 -0300

    create RuleMark and style axis of chart

--- Protfolio Project/HealthKitManager.swift
+++ Protfolio Project/HealthKitManager.swift
@@ -28,10 +28,13 @@ import Observation
                                                                options: .cumulativeSum,
                                                                anchorDate: endDate,
                                                                intervalComponents: .init(day: 1))
-        
-        let stepCounts = try! await stepsQuery.result(for: store)
-        stepData = stepCounts.statistics().map {
-            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        do {
+            let stepCounts = try await stepsQuery.result(for: store)
+            stepData = stepCounts.statistics().map {
+                .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+        } catch {
+            
         }
     }
     
@@ -47,10 +50,13 @@ import Observation
                                                                 options: .mostRecent,
                                                                 anchorDate: endDate,
                                                                 intervalComponents: .init(day: 1))
-        
-        let weights = try! await weightQuery.result(for: store)
-        weightData = weights.statistics().map {
-            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .gram()) ?? 0)
+        do {
+            let weights = try await weightQuery.result(for: store)
+            weightData = weights.statistics().map {
+                .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .gram()) ?? 0)
+            }
+        } catch {
+            
         }
     }
     
